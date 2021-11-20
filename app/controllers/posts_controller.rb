316f7e2a1996f517007478bba42d45d960cafc47
304089 <9457imai2@gmail.com>
2021-11-20 15:33:18 +0000

    youtube埋め込み

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,24 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    url = params[:post][:youtube_url]
+    url = url.last(11)
+    @post.youtube_url = url
+    @post.user_id = current_user.id
+    @post.save
+    redirect_to posts_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title,:introduction, :youtube_url)
+  end
 end
