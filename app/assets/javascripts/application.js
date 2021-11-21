cf2b9e699d75cc895fe8cc98cd57db00b02f671b
304089 <9457imai2@gmail.com>
2021-11-21 08:26:58 +0000

    マイページタブ（試作）

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,14 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+/*global $*/
+
+$(function() {
+  const tabs = $(".tab"); // tabのクラスを全て取得し、変数tabsに配列で定義
+  $(".tab").on("click", function() { // tabをクリックしたらイベント発火
+    $(".active").removeClass("active"); // activeクラスを消す
+    $(this).addClass("active"); // クリックした箇所にactiveクラスを追加
+    const index = tabs.index(this); // クリックした箇所がタブの何番目か判定し、定数indexとして定義
+    $(".content").removeClass("show").eq(index).addClass("show"); // showクラスを消して、contentクラスのindex番目にshowクラスを追加
+  })
+})
